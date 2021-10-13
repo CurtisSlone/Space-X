4e9739bd9c3d56d98430146c2273025680931f53
Curtis Slone <curtisdslone@gmail.com>
2021-10-13 12:37:08 +0200

    Style Update on header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App(){
     return(
         <div className="App">
             <Header />
-            
+            <h1> Content </h1>
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
-
+import '../style/Header.scss'
 const Header = () => {
     return(
         <div className="header">
             <div className="header__top">
                 <img className="header__logo" src="https://cdn.dribbble.com/users/610788/screenshots/5157282/spacex.png" alt="space-X" />
-                <p>Choose a campaign by</p>
+                
             </div>
             <div className="header__bottom">
                 <ul>
